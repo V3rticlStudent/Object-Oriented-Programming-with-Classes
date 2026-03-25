6472c2ac5f63e5782aa194903319402e38092747
V3rticlStudent <n10016079@humber.ca>
2026-03-25 17:08:17 -0400

    Add toString method for inventory items

--- inventory.js
+++ inventory.js
@@ -23,4 +23,7 @@ class PerishableProduct extends Product {
     super(name, price, quantity);
     this.expirationDate = expirationDate;
   }
+ toString() {
+    return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+  }
 }
