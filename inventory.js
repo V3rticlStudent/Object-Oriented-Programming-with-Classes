717db37f850c21e3d17993ef3a0f6712d53511f5
V3rticlStudent <n10016079@humber.ca>
2026-03-25 17:07:55 -0400

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -19,4 +19,8 @@ getTotalValue() {
 }
 
 class PerishableProduct extends Product {
+ constructor(name, price, quantity, expirationDate) {
+    super(name, price, quantity);
+    this.expirationDate = expirationDate;
+  }
 }
